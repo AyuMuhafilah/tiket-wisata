4aa272b82e955377fe349529c733749ebd4b111b
AyuMuhafilah <ayumuhafilah10@gmail.com>
2024-06-25 00:47:26 +0700

    Edit Bank

---
+++ app/Database/Seeds/Bank.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Bank extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'id_admin'   => '1',
+                'nama_bank'  => 'BRI',
+                'no_rek'     => '1234567891023456',
+            ],
+            [
+                'id_admin'   => '1',
+                'nama_bank'  => 'BNI',
+                'no_rek'     => '2345678910123456',
+            ],
+            [
+                'id_admin'   => '1',
+                'nama_bank'  => 'BCA',
+                'no_rek'     => '4567891012345678',
+            ],
+            [
+                'id_admin'   => '1',
+                'nama_bank'  => 'BSI',
+                'no_rek'     => '3456789101234567',
+            ],
+            [
+                'id_admin'   => '1',
+                'nama_bank'  => 'MANDIRI',
+                'no_rek'     => '5678910123456789',
+            ],
+            [
+                'id_admin'   => '2',
+                'nama_bank'  => 'BRI',
+                'no_rek'     => '1234567891023456',
+            ],
+            [
+                'id_admin'   => '2',
+                'nama_bank'  => 'BNI',
+                'no_rek'     => '2345678910123456',
+            ],
+            [
+                'id_admin'   => '2',
+                'nama_bank'  => 'BCA',
+                'no_rek'     => '4567891012345678',
+            ],
+            [
+                'id_admin'   => '2',
+                'nama_bank'  => 'BSI',
+                'no_rek'     => '3456789101234567',
+            ],
+            [
+                'id_admin'   => '2',
+                'nama_bank'  => 'MANDIRI',
+                'no_rek'     => '5678910123456789',
+            ],
+            [
+                'id_admin'   => '3',
+                'nama_bank'  => 'BRI',
+                'no_rek'     => '1234567891023456',
+            ],
+            [
+                'id_admin'   => '3',
+                'nama_bank'  => 'BNI',
+                'no_rek'     => '2345678910123456',
+            ],
+            [
+                'id_admin'   => '3',
+                'nama_bank'  => 'BCA',
+                'no_rek'     => '4567891012345678',
+            ],
+            [
+                'id_admin'   => '3',
+                'nama_bank'  => 'BSI',
+                'no_rek'     => '3456789101234567',
+            ],
+            [
+                'id_admin'   => '3',
+                'nama_bank'  => 'MANDIRI',
+                'no_rek'     => '5678910123456789',
+            ],
+            [
+                'id_admin'   => '4',
+                'nama_bank'  => 'BRI',
+                'no_rek'     => '1234567891023456',
+            ],
+            [
+                'id_admin'   => '4',
+                'nama_bank'  => 'BNI',
+                'no_rek'     => '2345678910123456',
+            ],
+            [
+                'id_admin'   => '4',
+                'nama_bank'  => 'BCA',
+                'no_rek'     => '4567891012345678',
+            ],
+            [
+                'id_admin'   => '4',
+                'nama_bank'  => 'BSI',
+                'no_rek'     => '3456789101234567',
+            ],
+            [
+                'id_admin'   => '4',
+                'nama_bank'  => 'MANDIRI',
+                'no_rek'     => '5678910123456789',
+            ],
+            [
+                'id_admin'   => '5',
+                'nama_bank'  => 'BRI',
+                'no_rek'     => '1234567891023456',
+            ],
+            [
+                'id_admin'   => '5',
+                'nama_bank'  => 'BNI',
+                'no_rek'     => '2345678910123456',
+            ],
+            [
+                'id_admin'   => '5',
+                'nama_bank'  => 'BCA',
+                'no_rek'     => '4567891012345678',
+            ],
+            [
+                'id_admin'   => '5',
+                'nama_bank'  => 'BSI',
+                'no_rek'     => '3456789101234567',
+            ],
+            [
+                'id_admin'   => '5',
+                'nama_bank'  => 'MANDIRI',
+                'no_rek'     => '5678910123456789',
+            ],
+            [
+                'id_admin'   => '6',
+                'nama_bank'  => 'BRI',
+                'no_rek'     => '1234567891023456',
+            ],
+            [
+                'id_admin'   => '6',
+                'nama_bank'  => 'BNI',
+                'no_rek'     => '2345678910123456',
+            ],
+            [
+                'id_admin'   => '6',
+                'nama_bank'  => 'BCA',
+                'no_rek'     => '4567891012345678',
+            ],
+            [
+                'id_admin'   => '6',
+                'nama_bank'  => 'BSI',
+                'no_rek'     => '3456789101234567',
+            ],
+            [
+                'id_admin'   => '6',
+                'nama_bank'  => 'MANDIRI',
+                'no_rek'     => '5678910123456789',
+            ],
+            [
+                'id_admin'   => '7',
+                'nama_bank'  => 'BRI',
+                'no_rek'     => '1234567891023456',
+            ],
+            [
+                'id_admin'   => '7',
+                'nama_bank'  => 'BNI',
+                'no_rek'     => '2345678910123456',
+            ],
+            [
+                'id_admin'   => '7',
+                'nama_bank'  => 'BCA',
+                'no_rek'     => '4567891012345678',
+            ],
+            [
+                'id_admin'   => '7',
+                'nama_bank'  => 'BSI',
+                'no_rek'     => '3456789101234567',
+            ],
+            [
+                'id_admin'   => '7',
+                'nama_bank'  => 'MANDIRI',
+                'no_rek'     => '5678910123456789',
+            ],
+            [
+                'id_admin'   => '8',
+                'nama_bank'  => 'BRI',
+                'no_rek'     => '1234567891023456',
+            ],
+            [
+                'id_admin'   => '8',
+                'nama_bank'  => 'BNI',
+                'no_rek'     => '2345678910123456',
+            ],
+            [
+                'id_admin'   => '8',
+                'nama_bank'  => 'BCA',
+                'no_rek'     => '4567891012345678',
+            ],
+            [
+                'id_admin'   => '8',
+                'nama_bank'  => 'BSI',
+                'no_rek'     => '3456789101234567',
+            ],
+            [
+                'id_admin'   => '8',
+                'nama_bank'  => 'MANDIRI',
+                'no_rek'     => '5678910123456789',
+            ],
+            [
+                'id_admin'   => '9',
+                'nama_bank'  => 'BRI',
+                'no_rek'     => '1234567891023456',
+            ],
+            [
+                'id_admin'   => '9',
+                'nama_bank'  => 'BNI',
+                'no_rek'     => '2345678910123456',
+            ],
+            [
+                'id_admin'   => '9',
+                'nama_bank'  => 'BCA',
+                'no_rek'     => '4567891012345678',
+            ],
+            [
+                'id_admin'   => '9',
+                'nama_bank'  => 'BSI',
+                'no_rek'     => '3456789101234567',
+            ],
+            [
+                'id_admin'   => '9',
+                'nama_bank'  => 'MANDIRI',
+                'no_rek'     => '5678910123456789',
+            ],
+        ];
+        $this->db->table('bank')->insertBatch($data);
+    }
+}
